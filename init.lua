78230df869b5a8b817230c76e7d810cdcb93cde4
sejjy <sejjymvm@gmail.com>
2025-07-30 07:02:19 +0800

    [refactor] change dirname from core to custom

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 require("config.options")
-require("core.lazy")
-require("core.diagnostics")
+require("custom.lazy")
+require("custom.diagnostics")
 require("config.keymaps")
 require("config.autocmds")
