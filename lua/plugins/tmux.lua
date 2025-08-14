1ccac8b5ebdae3a750dd88f7831dab83c09abb0b
sejjy <sejjymvm@gmail.com>
2025-08-15 02:36:50 +0800

    [fix] use <Cmd> for navigation

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -3,11 +3,11 @@ return {
 		"christoomey/vim-tmux-navigator",
 
 		keys = {
-			{ "<C-h>", ":<C-u>TmuxNavigateLeft<Enter>" },
-			{ "<C-j>", ":<C-u>TmuxNavigateDown<Enter>" },
-			{ "<C-k>", ":<C-u>TmuxNavigateUp<Enter>" },
-			{ "<C-l>", ":<C-u>TmuxNavigateRight<Enter>" },
-			{ "<C-\\>", ":<C-u>TmuxNavigatePrevious<Enter>" },
+			{ "<C-h>", "<Cmd><C-u>TmuxNavigateLeft<Enter>" },
+			{ "<C-j>", "<Cmd><C-u>TmuxNavigateDown<Enter>" },
+			{ "<C-k>", "<Cmd><C-u>TmuxNavigateUp<Enter>" },
+			{ "<C-l>", "<Cmd><C-u>TmuxNavigateRight<Enter>" },
+			{ "<C-\\>", "<Cmd><C-u>TmuxNavigatePrevious<Enter>" },
 		},
 	},
 
